b1c140749d2b43e93e163ae62854d26b304b1dd6
affankhan43 <affankhan627@gmail.com>
2018-06-05 11:11:35 +0500

    'conditions'

--- conditions.py
+++ conditions.py
@@ -1,8 +1,13 @@
-a, b = 0,1
+a, b = 1,1
 
 if a < b :
 	print('a ({}) is less than b ({})'.format(a,b))
+elif a == b:
+	print('a ({}) is equals to b ({})'.format(a,b))
 else:
 	print('a ({}) is greater than b ({})'.format(a,b))
 
+
+s = "less than" if a < b else "Not less than"
+print(s)
 print("foo" if a < b else "bar")
